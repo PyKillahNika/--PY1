944c2ac04497c3d04d33fb6575702c227c66b3c6
PyKillahNika <115494633+PyKillahNika@users.noreply.github.com>
2022-10-19 22:26:47 +0300

    Update task6.py

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -1,5 +1,11 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO Оформить решение
-list_numbers[list_numbers.index(max(list_numbers))], list_numbers[len(list_numbers)-1] = list_numbers[len(list_numbers)-1], list_numbers[list_numbers.index(max(list_numbers))]
+super_high = list_numbers[0]
+# ищем максимальное через цикл (не используем max())
+for each in list_numbers:
+    super_high = each * int(each > super_high) + super_high * int(not each > super_high)
+# реплейсим
+list_numbers[len(list_numbers)-1], list_numbers[list_numbers.index(super_high)] =\
+    list_numbers[list_numbers.index(super_high)], list_numbers[len(list_numbers)-1]
 print(list_numbers)
