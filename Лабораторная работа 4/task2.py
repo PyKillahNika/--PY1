09b2ce08677bdd73f1fcd05aab913e64b8cf7e5b
PyKillahNika <115494633+PyKillahNika@users.noreply.github.com>
2022-10-30 21:21:11 +0300

    Add files via upload

---
+++ Лабораторная работа 4/task2.py
@@ -0,0 +1,29 @@
+def get_count_char(str_):
+    str_ = str_.lower()
+    # TODO посчитать количество каждой буквы в строке в аргументе str_
+    slovar = dict()
+    for each in range(len(str_)):
+        if str_[each].isalpha():
+            if str_[each] in slovar:
+                slovar[str_[each]] += 1
+            else:
+                slovar[str_[each]] = 1
+    return slovar
+
+
+def percentil(dictionary):
+    total = 0
+    for each in dictionary:
+        total += dictionary[each]
+    for each in dictionary:
+        dictionary[each] = f'{dictionary[each]/total*100} %'
+    return dictionary
+
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
+print(percentil(get_count_char(main_str)))
---
+++ Лабораторная работа 4/task3.py
@@ -0,0 +1,12 @@
+def delete(list_, index=None):
+    if index is None:
+        list_ = list_[0:-1]
+    else:
+        list_ = list_[0:index] + list_[index+1:len(list_)]
+    return list_
+# TODO реализовать функцию удаления элемента из списка по индексу
+
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1, 2]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2, 3]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3, 4]
