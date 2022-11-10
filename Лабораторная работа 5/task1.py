595f53dbb4cf92ff44197e9ff0c5298af3ecd682
PyKillahNika <115494633+PyKillahNika@users.noreply.github.com>
2022-11-10 12:27:42 +0300

    Delete Лабораторная работа 5 directory

--- Лабораторная работа 5/task1.py
+++
@@ -1,4 +0,0 @@
-# TODO решить с помощью list comprehension и распечатать его
-import pprint as p
-n = 15  # наибольшее число для генерации (включительно)
-p.pprint([({"bin": bin(each), "dec": each, "hex": hex(each), "oct": oct(each)}) for each in range(n+1)])
