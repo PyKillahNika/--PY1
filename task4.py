eb151f416e02638fa6730ce60bef9deba31d3ea7
PyKillahNika <115494633+PyKillahNika@users.noreply.github.com>
2022-11-29 19:07:33 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -5,27 +5,14 @@ INPUT_FILE = "input.csv"
 def csv_to_list_dict(filename, delimiter=",", new_line="\n") -> list[dict]:
     with open(filename) as f:
         headers = list()
-        for line in f:
-            word = ""
-            for each in line:
-                if (each != delimiter) and (each != new_line[0]):
-                    word += each
-                else:
-                    headers.append(word)
-                    word = ""
+        for line_ in f:
+            headers = (line_[:-len(new_line)].split(delimiter))
             break
-        data = list()
-        for line in f:
-            current_line = list()
-            word = ""
-            for each in line:
-                if (each != delimiter) and (each != new_line[0]):
-                    word += each
-                else:
-                    current_line.append(word)
-                    word = ""
-            data.append(current_line)
-    return [{headers[list_.index(each)]: each for each in list_} for list_ in data]
+        itog = list()
+        for line_ in f:
+            line_ = line_[:-len(new_line)].split(delimiter)
+            itog.append({headers[line_.index(each)]: each for each in line_})
+    return itog
 
 
 print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
